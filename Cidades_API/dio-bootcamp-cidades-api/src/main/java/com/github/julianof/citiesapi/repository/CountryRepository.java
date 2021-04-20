9cda5376fdbb0da996fcd9141e2448ead94ca5d1
julianoferrone12 <62573072+julianoferrrone12@users.noreply.github.com>
2021-04-20 10:42:20 -0300

    Delete Cidades_API/dio-bootcamp-cidades-api directory

--- Cidades_API/dio-bootcamp-cidades-api/src/main/java/com/github/julianof/citiesapi/repository/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.julianof.citiesapi.repository;
-
-import com.github.julianof.citiesapi.entity.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, Long> {
-}
